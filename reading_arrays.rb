40ccb67a593552c3345138e1aba08c82e796fad8
Kyra-The-Unicorn <kyrastinson90@gmail.com>
2018-07-11 20:12:14 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -6,7 +6,7 @@ STUDENT_NAMES = [
   "Nicki Minaj",
   "Rihanna"
 ] 
-first_student_by_index.first
+first_student_by_index
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
